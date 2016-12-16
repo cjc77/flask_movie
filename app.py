43d77d296bf4eb9dd2b5ef7e4998601b869dd097
carson <carsonjamescook@gmail.com>
2016-12-16 00:01:29 -0800

    Basic database queries are set up.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 import secret
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:" + secret.pswd + "@localhost"
+app.debug = True
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -21,12 +22,17 @@ class User(db.Model):
 
 @app.route("/")
 def index():
-    return render_template("add_user.html", name=name)
+    all_users = User.query.all()
+    # Find a user by ID
+    first_user = User.query.get(1)
+    return render_template("add_user.html", all_users=all_users, first_user=first_user)
 
 @app.route("/post_user", methods=["POST"])
 def post_user():
-    return ""
-
+    user = User(request.form["username"], request.form["email"])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.run()
